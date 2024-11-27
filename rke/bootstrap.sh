9c9171134930c7408240c5de41bd0cfc021a00eb
ZouhairCharef <38728584+ZouhairCharef@users.noreply.github.com>
2024-11-27 14:16:34 +0000

    rke 1 vagrant file

---
+++ rke/bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo apt-get update -y >/dev/null 2>&1
+sudo apt-get upgrade -y >/dev/null 2>&1
+
+# Enable ssh password authentication
+echo "[TASK 1] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "[TASK 2] Set root password"
+echo -e "admin\nadmin" | passwd root >/dev/null 2>&1
+
+ufw disable
+
+swapoff -a; sed -i '/swap/d' /etc/fstab
+
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce=5:27.2.1-1~ubuntu.22.04~jammy docker-ce-cli=5:27.2.1-1~ubuntu.22.04~jammy containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-mark hold docker-ce docker-ce-cli containerd.io
